a35abe6d613dd66ccb6127d78ffb1574cbdd5ed1
Станислав Бабкин <Stanislav.Babkin@indusoft.ru>
2021-10-19 11:10:06 +0300

    Добавил scss, отправку данных на сервер

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { LoginPage } from './login/LoginPage';
+import { Routes, Route } from 'react-router';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/login">Log in</Link>
+        </nav>
+        <Routes>
+          <Route path="login" element={<LoginPage />}></Route>
+        </Routes>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ frontend/src/login/LoginPage.js
@@ -0,0 +1,63 @@
+import './LoginPage.scss';
+import { Form, Button } from 'react-bootstrap';
+import React from 'react';
+import axios from 'axios';
+
+export class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+        };
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('/api/login', {
+            username: this.state.username,
+            password: this.state.password,
+        }).then(response => console.log(response.data));
+    };
+
+    handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    render() {
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" placeholder="Enter username" 
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleInputChange} />
+                    <Form.Text className="text-muted">
+                        We'll never share your username with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleInputChange} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Log In
+                </Button>
+            </Form>
+        );
+    }
+}
