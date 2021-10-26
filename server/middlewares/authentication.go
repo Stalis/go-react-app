d6b8d15cf68e49dd6de3418b82dbc328f7cfbd1a
Stanislaw Babkin <stalissss@outlook.com>
2021-10-26 21:46:35 +0300

    Add session authentication

--- server/middlewares/authentication.go
+++ server/middlewares/authentication.go
@@ -4,39 +4,55 @@ import (
 	"go-react-app/server/dal"
 	"go-react-app/server/util/logger"
 	"net/http"
+	"net/url"
+	"strings"
 
-	"github.com/gofrs/uuid"
+	gsessions "github.com/gorilla/sessions"
 	"github.com/pkg/errors"
 )
 
 type authentication struct {
 	log      *logger.Logger
 	sessions dal.SessionRepository
+	store    gsessions.Store
 }
 
 func NewAuthentication(log *logger.Logger, sessions dal.SessionRepository) *authentication {
-	return &authentication{log, sessions}
+	return &authentication{log, sessions, gsessions.NewCookieStore([]byte{})}
 }
 
-func (auth *authentication) CheckToken(token string) error {
-	if token == "initial" {
-		auth.log.Debug().Msgf("Initial user")
+func (auth *authentication) CheckToken(r *http.Request) error {
+	uri := r.URL.RequestURI()
+	parsedUrl, _ := url.ParseRequestURI(uri)
+	parts := strings.Split(parsedUrl.Path, "/")
+	auth.log.Debug().Interface("parsed", parsedUrl).Msg("")
+	switch parts[len(parts)-1] {
+	case "login":
+	case "register":
 		return nil
 	}
 
-	session, err := auth.sessions.GetSessionByToken(uuid.FromStringOrNil(token))
+	session, err := auth.store.Get(r, "myapp-session")
 	if err != nil {
-		return err
+		return errors.Wrap(err, "token not found")
 	}
 
-	auth.log.Debug().Msgf("Authenticated user: %d", session.UserId)
+	userId := 0
+	if session.Values["userId"] != nil {
+		userId = session.Values["userId"].(int)
+	}
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		return errors.New("not authenticated")
+	}
+
+	auth.log.Debug().Msgf("Authenticated user: %d", userId)
 	return nil
 }
 
 func (auth *authentication) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		token := r.Header.Get("X-Session-Token")
-		if err := auth.CheckToken(token); err != nil {
+
+		if err := auth.CheckToken(r); err != nil {
 			err = errors.Wrap(err, "error while authenticate")
 
 			auth.log.Error().Stack().Caller().Err(err).Msg("")
